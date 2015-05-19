bd1c3d491d527058ce63dc607035f1580cd06d7c
Evan Sosenko <razorx@evansosenko.com>
2015-05-18 21:26:15 -0700

    Add users data bag to unit tests.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe 'cafe-core::default' do
-  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new do |_, server|
+      server.create_data_bag('users', 'sudoroot' => {})
+    end.converge(described_recipe)
+  end
 
   before(:each) do
     stub_command('which sudo')
@@ -16,9 +20,11 @@ describe 'cafe-core::default' do
   context 'when corresponding attribute is not enabled' do
     recipes.each do |recipe|
       it "does not configure #{recipe}" do
-        chef_run = ChefSpec::ServerRunner.new do |node|
+        chef_run = ChefSpec::ServerRunner.new do |node, server|
           node.set['cafe']['core'][recipe]['enable'] = false
+          server.create_data_bag('users', 'sudoroot' => {})
         end.converge(described_recipe)
+
         expect(chef_run).to_not include_recipe("cafe-core::#{recipe}")
       end
     end
@@ -27,8 +33,9 @@ describe 'cafe-core::default' do
   context 'when corresponding attribute is enabled' do
     recipes.each do |recipe|
       it "configures #{recipe}" do
-        chef_run = ChefSpec::ServerRunner.new do |node|
+        chef_run = ChefSpec::ServerRunner.new do |node, server|
           node.set['cafe']['core'][recipe]['enable'] = true
+          server.create_data_bag('users', 'sudoroot' => {})
         end.converge(described_recipe)
 
         expect(chef_run).to include_recipe("cafe-core::#{recipe}")
